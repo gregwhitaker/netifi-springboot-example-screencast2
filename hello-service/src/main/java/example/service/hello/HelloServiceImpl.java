af1c1364e3543aa6eee12501d69efcee91ff0ee5
gregwhitaker <gwhitake@gmail.com>
2019-12-13 18:28:46 -0800

    Implemented the hello-service

--- hello-service/src/main/java/example/service/hello/HelloServiceImpl.java
+++ hello-service/src/main/java/example/service/hello/HelloServiceImpl.java
@@ -5,7 +5,10 @@ import example.service.hello.protobuf.HelloResponse;
 import example.service.hello.protobuf.HelloService;
 import io.netty.buffer.ByteBuf;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Component
 public class HelloServiceImpl implements HelloService {
@@ -15,5 +18,15 @@ public class HelloServiceImpl implements HelloService {
         return Mono.fromSupplier(() -> HelloResponse.newBuilder()
                 .setMessage(String.format("Hello, %s!", message.getName()))
                 .build());
+    }
+
+    @Override
+    public Flux<HelloResponse> getHelloMessages(HelloRequest message, ByteBuf metadata) {
+        return Flux.just("Hello, %s!", "Hola, %s!", "Bonjour, %s!")
+                .delayElements(Duration.ofSeconds(1))
+                .map(fmt -> String.format(fmt, message.getName()))
+                .map(s -> HelloResponse.newBuilder()
+                        .setMessage(s)
+                        .build());
     }
 }
